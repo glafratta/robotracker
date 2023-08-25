f51d203a897c9d33cd21b40219fb109bbf4e3516
Bernd Porr <mail@berndporr.me.uk>
2023-08-25 12:04:27 +0100

    Working now with tracking the bright spot!

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # robotracker
+
+![alt tag](screenshot.png)
+
+run with for example:
+
+```
+python optical_flow.py vids/culdesacA2
+```
+
+Press <Esc> to stop the tracking.
+
+It writes out:
+
+ - 1st image of the track (culdesacA2_1stframe.png)
+ - the tracking coordinates (culdesacA2_coord.txt)
+ - the trace as an image (culdesacA2_trace.png)
+
--- optical_flow.py
+++ optical_flow.py
@@ -4,19 +4,26 @@
 import numpy as np
 import cv2 as cv
 import argparse
+import csv
 
-parser = argparse.ArgumentParser(description='This sample demonstrates Lucas-Kanade Optical Flow calculation. \
-                                              The example file can be downloaded from: \
-                                              https://www.bogotobogo.com/python/OpenCV_Python/images/mean_shift_tracking/slow_traffic_small.mp4')
-parser.add_argument('image', type=str, help='path to image file')
+minY = 0
+minX = 610
+width = 755
+height = 917
+
+def doCrop(img):
+    return img[minY:minY+height, minX:minX+width]
+
+parser = argparse.ArgumentParser(description='Lucas-Kanade Optical Flow calculation.')
+parser.add_argument('image', type=str, help='path to image file without .mp4')
 args = parser.parse_args()
 
-cap = cv.VideoCapture(args.image)
+cap = cv.VideoCapture(args.image+".mp4")
 
 # params for ShiTomasi corner detection
 feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 7,
+                       qualityLevel = 0.5,
+                       minDistance = 70,
                        blockSize = 7 )
 
 # Parameters for lucas kanade optical flow
@@ -29,14 +36,22 @@ color = np.random.randint(0, 255, (100, 3))
 
 # Take first frame and find corners in it
 ret, old_frame = cap.read()
+old_frame = doCrop(old_frame)
+cv.imwrite(args.image+"_1stframe.png",old_frame)
+
 old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
 p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
 
+f = open(args.image+"_coord.txt","a")
+
+framenumber = 0
+
 while(1):
     ret, frame = cap.read()
+    frame = doCrop(frame)
     if not ret:
         print('No frames grabbed!')
         break
@@ -51,13 +66,18 @@ while(1):
         good_new = p1[st==1]
         good_old = p0[st==1]
 
-    # draw the tracks
+    f.write(str(framenumber))
+    framenumber = framenumber + int(1)
+
     for i, (new, old) in enumerate(zip(good_new, good_old)):
         a, b = new.ravel()
+        f.write("\t{}\t{}".format(a,b))
         c, d = old.ravel()
         mask = cv.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
         frame = cv.circle(frame, (int(a), int(b)), 5, color[i].tolist(), -1)
     img = cv.add(frame, mask)
+
+    f.write("\n")
 
     cv.imshow('frame', img)
     k = cv.waitKey(30) & 0xff
@@ -69,3 +89,7 @@ while(1):
     p0 = good_new.reshape(-1, 1, 2)
 
 cv.destroyAllWindows()
+
+f.close()
+
+cv.imwrite(args.image+"_trace.png",mask)
